d9f1e07ed1939155594afbb91fc1639168edf6b1
leadermags <maggie.11791@gmail.com>
2021-02-16 23:50:23 -0500

    updated Phone-and-Email-Scraper.py and added a .bat file
    cleaned up code and uses .bat file to allow user to fun via Windows+R

---
+++ Phone-and-Email-Scraper.bat
@@ -0,0 +1,2 @@
+@py C:\Users\school\Desktop\GitHub\personal-data-scraper-project\Phone-and-Email-Scraper.py %*
+@pause
--- Phone-and-Email-Scraper.py
+++ Phone-and-Email-Scraper.py
@@ -6,14 +6,17 @@ import re
 import openpyxl, os
 
 def main():
-  getLink()
+  print('What do you want to save your file as? ')
+  fileName = input() + '.xlsx'
+
+  getLink(fileName)
   
   location = os.getcwd()
   print('Emails and Phone Numbers extracted. ')
-  print('Data saved in ' + location + ' path.')
+  print('Data saved in ' + location + '\\' + fileName)
 
 # Extract links
-def getLink():
+def getLink(fileName):
   # Create a regex for webpages
   linkRegex = re.compile(r'''
 
@@ -27,31 +30,36 @@ def getLink():
   for l in extractedLinks:
     allLinks.append(l[0])
   wb = openpyxl.Workbook()
-  sheet = wb.get_sheet_by_name('Sheet')
+  sheet = wb['Sheet']
   sheet.title = 'All Links'
 
   i = 1
-  for links in allLinks:
+  for link in allLinks:
     cellNumber = 'A' + str(i)
-    sheet[cellNumber] = str(links)
+    sheet[cellNumber] = str(link)
     wb.create_sheet(str(i))
-    getSource(links, wb, str(i))
+    text = getSource(link)
+    
+    # Get the text off the clipboard
+    copiedText = pyperclip.paste()
+    extractedPhone = extractPhone(copiedText)
+    extractedEmail = extractEmail(copiedText)
+    pasteText(extractedPhone, extractedEmail, link, wb, str(i))
     i += 1
-    wb.save('extractedData.xlsx')
+    wb.save(fileName)
 
 # Copy text from links' html using bs4
-def getSource(link, wb, sh):
+def getSource(link):
+  try:
+    res = requests.get(str(link))
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('body')
+    return pyperclip.copy(str(elems))
+  except:
+    print('Could not get ' + link + ' webpage')
   
-  res = requests.get(str(link))
-  res.raise_for_status()
-  soup = bs4.BeautifulSoup(res.text, 'html.parser')
-  elems = soup.select('body')
-  pyperclip.copy(str(elems))
-  extractInfo(link, wb, sh)
-
-# Extract emails and phone numbers
-def extractInfo(link, wb, sh):
-
+def extractPhone(copiedText):
   # Create a regex for phone numbers
   phoneRegex = re.compile(r'''
   # 000-000-0000, 000-0000, (000) 000-0000, 000-0000 ext 12345, ext. 12345, x12345
@@ -65,7 +73,16 @@ def extractInfo(link, wb, sh):
   (\d{2,5}))?                         # number part for extension (optional)
   )
   ''', re.VERBOSE)
+  extractedPhone = phoneRegex.findall(copiedText)
+  
+  allPhoneNumbers = []
+  for phoneNumber in extractedPhone:
+      allPhoneNumbers.append(phoneNumber[0])
 
+  return allPhoneNumbers
+
+def extractEmail(copiedText):
+  
   # Create a regex for emails
   emailRegex = re.compile(r'''
   # some.+_thing@something.com
@@ -75,24 +92,15 @@ def extractInfo(link, wb, sh):
   [a-zA-Z0-9_.+-]+                 # domain name
 
   ''', re.VERBOSE)
-
-  # Get the text off the clipboard
-  copiedText = pyperclip.paste()
-
-  # Extract the email/phone from this text
-  extractedPhone = phoneRegex.findall(copiedText)
   extractedEmail = emailRegex.findall(copiedText)
-  allPhoneNumbers = []
-  for phoneNumber in extractedPhone:
-      allPhoneNumbers.append(phoneNumber[0])
-  pasteText(allPhoneNumbers, extractedEmail, link, wb, sh)
+  return extractedEmail
 
 # Input data into spreadsheet
 def pasteText(allPhoneNumbers, extractedEmail, link, wb, sh):
 
-  sheet = wb.get_sheet_by_name(sh)
+  sheet = wb[sh]
 
-  for j in range(0, len(extractedEmail)):
+  for j in range(0, len(extractedEmail) + len(allPhoneNumbers)):
     cellNumber = 'A' + str(j + 1)
     sheet[cellNumber] = link
 
@@ -102,12 +110,9 @@ def pasteText(allPhoneNumbers, extractedEmail, link, wb, sh):
     sheet[cellNumber] = str(e)
     i += 1
 
-  i = 1
   for p in allPhoneNumbers:
     cellNumber = 'C' + str(i)
     sheet[cellNumber] = str(p)
     i += 1
-    
-  wb.save('extractedData.xlsx')
 
 main()
